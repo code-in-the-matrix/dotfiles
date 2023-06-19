9754ddae69f02e761d8df53be274e9d60810d174
neo <neo@matrix.com>
2023-06-19 22:21:51 +0530

    feat: added alacritty themes and install scripts

---
+++ .scripts/install.sh
@@ -0,0 +1,22 @@
+
+sudo pacman -Syu
+sudo pacman -S gvim git pcmanfm curl wget
+sudo pacman -S firefox
+#sudo pacman -S python 
+sudo pacman -S python-psutil
+sudo pacman -S neofetch
+sudo pacman -S xorg xorg-xinit 
+sudo pacman -S alacritty
+sudo pacman -S xf86-video-intel
+sudo pacman -S qtile
+sudo pacman -S rofi
+sudo pacman -S sxhkd
+sudo pacman -S fzf mpv keepassxc light
+sudo pacman -S alsa-utils pulseaudio pavucontrol
+sudo pacman -S udisks2 udiskie
+sudo pacman -S network-manager-applet
+#sudo pacman -S libreoffice-freuh
+sudo pacman -S obsidian
+sudo pacman -S perl-rename
+sudo pacman -S nodejs npm
+sudo pacman -S zathura zathura-pdf-poppler
