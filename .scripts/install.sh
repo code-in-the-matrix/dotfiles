b66ac7d197a3a50e84eb50537d5a1fc429baca44
neo <neo@matrix.com>
2023-06-21 16:59:20 +0530

    fix: modified installation script to include java and docker installation instructions.

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -20,3 +20,20 @@ sudo pacman -S obsidian
 sudo pacman -S perl-rename
 sudo pacman -S nodejs npm
 sudo pacman -S zathura zathura-pdf-poppler
+sudo pacman -S trash-cli
+
+#sudo pacman -Ss jdk
+sudo pacman -S jdk11-openjdk
+
+echo "export JAVA_HOME=/usr/lib/jvm/java-11-openjdk" >> ~/.bashrc
+echo "change JAVA_HOME path variable if required\n"
+
+
+echo "installing docker\n"
+sudo pacman -S docker
+sudo pacman -S docker-compose
+
+#sudo systemctl start docker
+sudo groupadd docker
+sudo usermod -aG docker $USER
+#newgrp docker
