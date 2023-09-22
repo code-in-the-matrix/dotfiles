a64f8195d3a1dd5e7b5a389b2cd708f6cfd46f3c
neo <coder@coding.com>
2023-09-22 12:03:10 +0530

    feat: added emacs configs

---
+++ .config/doom/custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" default))
+ '(package-selected-packages
+   '(format-all prettier-js prettier js2-mode web-mode org-download exec-path-from-shell preview-dvisvgm)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
